e4c4ad84937d3f3209269817d50a6dccb4d203c9
Oluwatomisin <malcolmtomisin@gmail.com>
2020-05-25 20:03:31 +0100

    firebase activity

---
+++ app/src/main/java/com/pausemedia/hippo/FirebaseActivity.java
@@ -0,0 +1,80 @@
+package com.pausemedia.hippo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
+
+public class FirebaseActivity extends AppCompatActivity {
+    private SharedPreferences preferences;
+    private static final String user = "com.pausemedia.hippo.USER";
+    private static final String firebaseUser = "user";
+    private static final int RC_SIGN_IN = 123;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+        preferences = this.getSharedPreferences(user, Context.MODE_PRIVATE);
+    }
+
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.TwitterBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+        // [END auth_fui_create_intent]
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Gson gson = new Gson();
+                Type type = new TypeToken<FirebaseUser>(){}.getType();
+                String json = gson.toJson(user, type);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(firebaseUser,json);
+                editor.apply();
+                Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT).show();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                if (resultCode != RESULT_CANCELED)
+                Toast.makeText(this, "Error Signing in", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
