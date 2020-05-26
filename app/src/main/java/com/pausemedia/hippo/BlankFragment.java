c92b9e12801f4f579784ed1dc153ba7f1e0b10d2
Oluwatomisin <malcolmtomisin@gmail.com>
2020-05-26 03:27:35 +0100

    stubbing up fragment

--- app/src/main/java/com/pausemedia/hippo/BlankFragment.java
+++ app/src/main/java/com/pausemedia/hippo/BlankFragment.java
@@ -5,7 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.pausemedia.hippo.databinding.FragmentPromptBinding;
+import com.pausemedia.hippo.model.Script;
 
 
 /**
@@ -18,6 +24,8 @@ public class BlankFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    Script scriptViewModel;
+    FragmentPromptBinding binding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,6 +66,16 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        binding = inflater.inflate(R.layout.fragment_prompt, container);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        scriptViewModel = new ViewModelProvider(requireActivity()).get(Script.class);
+        scriptViewModel.getReport().observe(getViewLifecycleOwner(), script -> {
+            binding.reportText.setText(script);
+        });
     }
 }
